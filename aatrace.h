5d81daadbb7f17f544f3709d420eac2f357e77cb
Sergey Kvachonok <ravenexp@gmail.com>
2012-05-15 10:43:15 +0300

    Expand README and document some changeable parameters in aatrace.h

--- aatrace.h
+++ aatrace.h
@@ -1,10 +1,18 @@
 #define AATRACE_FONT_WIDTH  8
 #define AATRACE_FONT_HEIGHT 16
 
+/* ASCII code of the first font character */
 #define AATRACE_FONT_ASCII_OFFSET 32
 
+/* Gradiant map scale adjustment
+   1 ::= normalized to the byte range
+*/
 #define AATRACE_DIFF_SCALE 4
 
+/* Differentiation kernel implementation
+   diff2 - 2x2 flat top
+   diff3 - 3x3 Sobel operator
+ */
 #define aatrace_diff aatrace_diff3
 
 typedef unsigned char pix_t;
