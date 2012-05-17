45dac4a99637d635b23c0c4d234fe0870356632d
Sergey Kvachonok <ravenexp@gmail.com>
2012-05-17 18:37:35 +0300

    Use unsigned ints where division by a constant is needed.
    Save a few ops needed to properly round shift results for the signed ints.

--- diff.c
+++ diff.c
@@ -4,11 +4,12 @@
 void aatrace_diff2(pix_t* d, pix_t* s, int w, int h)
 {
 	int l, i;
+	unsigned int D;
 
 	for (l = 0; l < h - 1; l++)
 		for (i = l*w; i < (l + 1)*w - 1; i++) {
-			int D = abs(s[i + w] - s[i] + s[i + w + 1] - s[i + 1])
-			      + abs(s[i + 1] - s[i] + s[i + w + 1] - s[i + w]);
+			D = abs(s[i + w] - s[i] + s[i + w + 1] - s[i + 1])
+			  + abs(s[i + 1] - s[i] + s[i + w + 1] - s[i + w]);
 
 			D = (D*AATRACE_DIFF_SCALE)/4;
 			d[i] = (D > 255) ? 255 : D;
@@ -18,16 +19,17 @@ void aatrace_diff2(pix_t* d, pix_t* s, int w, int h)
 void aatrace_diff3(pix_t* d, pix_t* s, int w, int h)
 {
 	int l, i;
+	unsigned int D;
 
 	for (l = 1; l < h - 1; l++)
 		for (i = l*w + 1; i < (l + 1)*w - 1; i++) {
-			int D = abs(2*(s[i + w + 0] - s[i - w + 0])
-				    +  s[i + w - 1] - s[i - w - 1]
-				    +  s[i + w + 1] - s[i - w + 1])
-				+
-				abs(2*(s[i + 0 + 1] - s[i + 0 - 1])
-				    +  s[i - w + 1] - s[i - w - 1]
-				    +  s[i + w + 1] - s[i + w - 1]);
+			D = abs(2*(s[i + w + 0] - s[i - w + 0])
+				+  s[i + w - 1] - s[i - w - 1]
+				+  s[i + w + 1] - s[i - w + 1])
+			    +
+			    abs(2*(s[i + 0 + 1] - s[i + 0 - 1])
+				+  s[i - w + 1] - s[i - w - 1]
+				+  s[i + w + 1] - s[i + w - 1]);
 
 			D = (D*AATRACE_DIFF_SCALE)/8;
 			d[i] = (D > 255) ? 255 : D;
@@ -54,11 +56,11 @@ void aatrace_diff4(pix_t* d, pix_t* s, int w, int h)
 {
 	int l, i;
 	int kl, kc;
+	unsigned int D;
 
 	for (l = 1; l < h - 2; l++)
 		for (i = l*w + 1; i < (l + 1)*w - 2; i++) {
 			int Dx = 0, Dy = 0;
-			int D;
 
 			for (kl = 0; kl < 4; kl++)
 				for (kc = 0; kc < 4; kc++) {
@@ -69,7 +71,6 @@ void aatrace_diff4(pix_t* d, pix_t* s, int w, int h)
 			}
 
 			D = abs(Dx) + abs(Dy);
-
 			D = (D*AATRACE_DIFF_SCALE)/24;
 			d[i] = (D > 255) ? 255 : D;
 		}
