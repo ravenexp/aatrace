01ff60741e59bda442d4ef006588baf6de65e270
Sergey Kvachonok <ravenexp@gmail.com>
2012-05-30 16:16:06 +0300

    Remove diff and render stages from testconv utility.
    Use testdiff to produce gradient maps and testrender to draw text.

--- testconv.c
+++ testconv.c
@@ -9,50 +9,33 @@ int main(int argc, char* argv[])
 {
 	pix_t* fbuf;
 	pix_t* sbuf;
-	pix_t* dbuf;
-	pix_t* obuf;
 	int width, height;
 	int fwidth, fheight;
 
 	char* txtbuf;
 	int twidth, theight;
 
-	if (argc < 3)
+	if (argc < 4)
 		return 1;
 
 	sbuf = util_load_pic(argv[1], &width, &height);
 	if (!sbuf)
 		return 2;
 
-	dbuf = (pix_t*)malloc(width*height);
-	aatrace_diff(dbuf, sbuf, width, height);
-	util_store_pic(argv[2], dbuf, width, height);
-
-	if (argc < 5)
-		return 3;
-
-	fbuf = util_load_pic(argv[3], &fwidth, &fheight);
+	fbuf = util_load_pic(argv[2], &fwidth, &fheight);
 	if (!fbuf || fwidth != AATRACE_FONT_WIDTH)
-		return 4;
+		return 3;
 
 	twidth = width/AATRACE_FONT_WIDTH;
 	theight = height/AATRACE_FONT_HEIGHT;
 
 	txtbuf = (char*)malloc(twidth*theight);
-	aatrace_match_pic(txtbuf, dbuf, fbuf,
+	aatrace_match_pic(txtbuf, sbuf, fbuf,
 			  width, height,
 			  fwidth, fheight, AATRACE_FONT_HEIGHT);
 
-	util_store_text(argv[4], txtbuf, twidth, theight);
-
-	obuf = (pix_t*)malloc(width*height);
-	aatrace_render_pic(obuf, txtbuf, fbuf,
-			   width,
-			   twidth, theight,
-			   fwidth, AATRACE_FONT_HEIGHT);
-
-	if (util_store_pic(argv[5], obuf, width, height) < 0)
-		return 5;
+	if (util_store_text(argv[3], txtbuf, twidth, theight) < 0)
+		return 4;
 
 	return 0;
 }
