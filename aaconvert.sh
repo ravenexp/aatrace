bfaa7749c5214ec99affe69f9c4d3c8d1c1df995
Sergey Kvachonok <ravenexp@gmail.com>
2012-05-31 11:36:43 +0300

    Add aaconvert.sh wrapper script.
    Greatly simplifies command chaining and filename generation.
    Specify aaconvert.sh as the main user interface in README.

---
+++ aaconvert.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+P=$(dirname "$0")
+S="$1"
+FONT=${AAFONT:-${P}/font/font8x16.pgm}
+
+[[ -f $S ]] || {
+    echo "Usage:"
+    echo "    aaconvert image.pgm"
+    exit
+} >&2
+
+[[ -f $FONT ]] || {
+    echo "No font file: '$FONT'"
+    echo "Check AAFONT= env var"
+    exit
+} >&2
+
+BS=$(basename "$S" ".pgm")
+
+D="${BS}.diff.pgm"
+T="${BS}.txt"
+O="${T}.pgm"
+
+die() {
+    echo "FAIL! Status: $?"
+    exit
+} >&2
+
+echo "DIFF:   $S -> $D"
+${P}/testdiff "$S" "$D" || die
+echo "FONT:   $FONT"
+echo "CONV:   $D -> $T"
+${P}/testconv "$D" "$FONT" "$T" || die
+echo "RENDER: $T -> $O"
+${P}/testrender "$T" "$FONT" "$O" || die
