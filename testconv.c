95a3723352b210945619ccf15862ff4c05564158
Sergey Kvachonok <ravenexp@gmail.com>
2012-08-19 17:29:37 +0300

    Use command line switches for options.
    Introduces hard getopt() dependency in testdiff and testconv.
    Even mingw seems to have it these days though...
    The rest of the code is still ISO C89.

--- testconv.c
+++ testconv.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
 
 #include "pnm.h"
 #include "aatrace.h"
 #include "util.h"
+
+static const char usage[] =
+"Usage:\n"
+"    testconv [-m <id>] [-s <id>] [-c <1|0>] [-W <sad>.<asd>]"
+" input.pgm font.pgm output.pgm";
 
 int main(int argc, char* argv[])
 {
@@ -12,36 +19,58 @@ int main(int argc, char* argv[])
 	struct aatrace_text txt;
 
 	struct aatrace_convert_ctx ctx;
+	int c;
 
 	aatrace_convert_ctx_init(&ctx);
 
-	if (argc < 4)
-		return 1;
+	while ((c = getopt(argc, argv, ":m:s:c:W:")) != -1) {
+		const char* p;
 
-	if (util_load_pic(&src, argv[1]) < 0)
-		return 2;
+		switch(c) {
+		case 'm':
+			ctx.match.method = (enum aatrace_match_method)atoi(optarg);
+			break;
+		case 's':
+			ctx.search.method = (enum aatrace_search_method)atoi(optarg);
+			break;
+		case 'c':
+			if (atoi(optarg)) {
+				ctx.search.flags |= AATRACE_SEARCH_FLAG_COVERAGE;
+			} else {
+				ctx.search.flags &= ~AATRACE_SEARCH_FLAG_COVERAGE;
+			}
+			break;
+		case 'W':
+			ctx.match.sad_weight = atoi(optarg);
+			p = strchr(optarg, '.');
+			if (p)
+				ctx.match.asd_weight = atoi(p + 1);
+			break;
+		case ':':
+			fprintf(stderr, "Option -%c requires an operand\n", optopt);
+			return 2;
+		default:
+		case '?':
+			fprintf(stderr, "Unrecognized option: -%c\n", optopt);
+			return 1;
+		}
+	}
 
-	if (util_load_font(&font, argv[2]) < 0)
+	if (optind != argc - 3) {
+		puts(usage);
 		return 3;
+	}
 
-	if (argc > 4)
-		ctx.match.method = (enum aatrace_match_method)atoi(argv[4]);
+	if (util_load_pic(&src, argv[optind]) < 0)
+		return 4;
 
-	if (argc > 5)
-		ctx.search.method = (enum aatrace_search_method)atoi(argv[5]);
-
-	if (argc > 6)
-		ctx.search.flags = atoi(argv[6]);
-
-	if (argc > 7)
-		ctx.match.sad_weight = atoi(argv[7]);
-	if (argc > 8)
-		ctx.match.asd_weight = atoi(argv[8]);
+	if (util_load_font(&font, argv[optind + 1]) < 0)
+		return 5;
 
 	aatrace_convert_pic(&txt, &src, &font, &ctx);
 
-	if (util_store_text(argv[3], &txt) < 0)
-		return 4;
+	if (util_store_text(argv[optind + 2], &txt) < 0)
+		return 6;
 
 	return 0;
 }
--- testdiff.c
+++ testdiff.c
@@ -1,31 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <getopt.h>
+
 #include "pnm.h"
 #include "aatrace.h"
 #include "util.h"
+
+static const char usage[] =
+"Usage:\n"
+"    testdiff [-l <scale>] [-d <kernel>] input.pgm output.pgm";
 
 int main(int argc, char* argv[])
 {
 	struct aatrace_pic src, dst;
 	struct aatrace_diff_ctx ctx;
+	int c;
 
 	aatrace_diff_ctx_init(&ctx);
 
-	if (argc < 3)
-		return 1;
+	while ((c = getopt(argc, argv, ":l:d:")) != -1) {
+		switch(c) {
+		case 'l':
+			ctx.scale = atoi(optarg);
+			break;
+		case 'd':
+			ctx.kernel = (enum aatrace_diff_kernel)atoi(optarg);
+			break;
+		case ':':
+			fprintf(stderr, "Option -%c requires an operand\n", optopt);
+			return 2;
+		default:
+		case '?':
+			fprintf(stderr, "Unrecognized option: -%c\n", optopt);
+			return 1;
+		}
+	}
 
-	if (util_load_pic(&src, argv[1]) < 0)
-		return 2;
+	if (optind != argc - 2) {
+		puts(usage);
+		return 3;
+	}
 
-	if (argc > 3)
-		ctx.scale = atoi(argv[3]);
-
-	if (argc > 4)
-		ctx.kernel = (enum aatrace_diff_kernel)atoi(argv[4]);
+	if (util_load_pic(&src, argv[optind]) < 0)
+		return 4;
 
 	aatrace_diff(&dst, &src, &ctx);
-	util_store_pic(argv[2], &dst);
+
+	if (util_store_pic(argv[optind + 1], &dst) < 0)
+		return 5;
 
 	return 0;
 }
